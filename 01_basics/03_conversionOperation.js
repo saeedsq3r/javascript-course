3f9e2c2dcfb94a4d308b6732ef533cac45daad95
saeedsq3r <saeedsq3r@gmail.com>
2026-05-26 09:59:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = 'saeed'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let value_in_number = Number(score)
+// console.log(typeof(value_in_number));
+
+// console.log(value_in_number);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let is_login = "saeed"
+
+let boolean_is_login = Boolean(is_login)
+// console.log(boolean_is_login);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "saeed" => true
+
+
+let some_number = 33
+
+let string_number = String(some_number)
+console.log(string_number);
+console.log(typeof(string_number));
+
+
+
