6c86c256407add68a62121cb1823445d194448cc
saeedsq3r <saeedsq3r@gmail.com>
2026-05-26 10:15:14 +0000

    operations on datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,8 +29,51 @@ let boolean_is_login = Boolean(is_login)
 let some_number = 33
 
 let string_number = String(some_number)
-console.log(string_number);
-console.log(typeof(string_number));
+// console.log(string_number);
+// console.log(typeof(string_number));
+
+
+
+// ***************************************************** Operations **********************************************
+
+let value = 3
+let neg_value = -value
+
+// console.log(neg_value);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = 'saeed'
+let str2 = ' khan'
+
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + '2');
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+tr`ue);
+// console.log(+"");`
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let game_counter = 100
+
+game_counter++
+console.log(game_counter);
 
 
 
