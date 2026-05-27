9f4b7821dcbc4b59bc6be8ab2cd18ddaee4c73ca
saeedsq3r <saeedsq3r@gmail.com>
2026-05-27 15:23:02 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(1000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const other_number = 123.8966
+
+// console.log(other_number.toPrecision(4))
+
+const num = 1000000
+// console.log(num.toLocaleString('en-IN'));
+
+// Number.MAX_VALUE
+// Number.MIN_VALUE
+
+//+++++++++++++++++++ Maths +++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(2,5,6,7,8));
+// console.log(Math.max(2,5,6,7,8));
+
+console.log(Math.random());
+console.log(((Math.random()*10) + 1).toFixed(0));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
