df2449896b681717a384318208a53f4347275d38
saeedsq3r <saeedsq3r@gmail.com>
2026-05-28 17:01:16 +0000

    rest operator in functions and passing objects, arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -57,6 +57,41 @@ function login_user_message(user_name = "Muhammad"){
 // empty string and undefined as known as false values
 
 
-console.log(login_user_message("saeed"))
+// console.log(login_user_message("saeed"))
 // console.log(login_user_message(""))
 // console.log(login_user_message())
+
+
+function calculate_cart_price(val1, val2, ...num1){ // ... is known as rest operator and spread operator according situation
+    return num1
+}
+
+// console.log(calculate_cart_price(200, 400, 500,2000))
+
+const user = {
+    user_name: 'saeed',
+    price: 199
+}
+
+function handle_object(anyobject){
+    console.log(`Username is ${anyobject.user_name} and price is ${anyobject.price}`);
+    
+}
+
+handle_object(user)
+handle_object({
+    user_name: 'saeedkhan',
+    price: 399
+})
+
+const my_new_arr = [200, 500, 100, 600]
+
+function return_second_value(arr){
+    return arr[0]
+    
+}
+
+// console.log(return_second_value(my_new_arr));
+console.log(return_second_value([2000, 400, 500, 600]));
+
+
