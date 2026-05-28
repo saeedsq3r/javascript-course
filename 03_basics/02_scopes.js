ee2817ae803c7ae49ce0de270fe87f0e253cdba7
saeedsq3r <saeedsq3r@gmail.com>
2026-05-28 17:30:46 +0000

    scopes ranges and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,7 +9,7 @@ if (true) {
     let a = 10
     const b = 20
     // var c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 
@@ -21,7 +21,7 @@ if (true) {
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
@@ -30,3 +30,55 @@ console.log(a);
 /* 
 in the inspect the global scope is different from the node scope 
 */
+
+
+function one(){
+    const username = "saeed"
+
+    function two(){
+        const website = "youtube"
+        console.log(username); // child use the parrent property/variable
+        
+    }
+    console.log(website); // error: parrent didn't use the child property/variable
+    
+    two()
+}
+
+// one()
+
+
+if (true) {
+    const username = 'saeed'
+    if (username === "saeed") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+// ++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+// console.log(addone(5))
+
+
+// addtow(5) // error: because of hoisting 
+
+const addtow = function(num){
+    return num + 2
+}
