fe4895a2f597257b7e7473ab46dd6356bd497385
saeedsq3r <saeedsq3r@gmail.com>
2026-05-29 15:58:50 +0000

    for loop , break & continue statements

---
+++ 05_iterations/one.js
@@ -0,0 +1,63 @@
+// for
+
+/*
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is best number");
+    }
+    console.log(element);
+    
+}
+*/
+
+/*
+for (let i = 1; i <= 10; i++) {
+    console.log(`Outer loop value: ${i}`);
+    
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and outer loop ${i}`);
+        console.log(i + '*' + j + ' = ' + i*j);
+        
+        
+    }
+    
+}
+*/
+
+// `` is back ticks
+
+let my_arr = ['maaz', 'saeed', 'junaid']
+// console.log(my_arr.length);
+
+
+for (let index = 0; index < my_arr.length; index++) {
+    const element = my_arr[index];
+    // console.log(element);
+    
+    
+}
+
+// key words: break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+
+//     console.log(`Value of i is ${i}`);
+    
+    
+// }
+
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+
+    console.log(`Value of i is ${i}`);
+    
+}
