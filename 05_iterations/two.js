c046a552c3e8aa740049bb5d92a808fe48dc74f7
saeedsq3r <saeedsq3r@gmail.com>
2026-05-29 16:13:00 +0000

    while loop and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while
+
+let i = 0
+while (i <= 10) {
+    //console.log(`Value of index is ${i}`);
+    i += 2
+}
+
+let my_arr = ['maaz', 'saeed', 'junaid']
+let arr = 0
+
+while (arr < my_arr.length) {
+    // console.log(`Value is ${my_arr[i]}`);
+    arr++
+}
+
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
